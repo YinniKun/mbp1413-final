d0227c8c3190337a211fcda594dbf62a1379030f
YinniKun <dongrichard123@gmail.com>
2024-02-19 17:56:12 -0500

    add dataloader

--- models/utils.py
+++ models/utils.py
@@ -10,6 +10,22 @@ Copyright (c) 2024 by Chris Xiao yl.xiao@mail.utoronto.ca, All Rights Reserved.
 '''
 import monai
 from monai.data import DataLoader
+from monai.transforms import (
+    AsDiscrete,
+    AsDiscreted,
+    EnsureChannelFirstd,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    SaveImaged,
+    ScaleIntensityRanged,
+    Spacingd,
+    RandAffined,
+    Resized,
+    Lambdad
+)
 import torch
 import torch.nn as nn
 import glob
@@ -24,6 +40,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import SimpleITK as sitk
 from pathlib import Path
+from omegaconf import OmegaConf
 
 ROOT = Path(os.path.dirname(os.path.realpath(__file__))).parent
 
@@ -138,10 +155,10 @@ def map_dataset(
             mask[mask != 0] = 255
             if m == 1:
                 combined_label = mask
-                pass
-            combined_label += mask
-            m += 1
-        
+                m += 1
+            else:
+                combined_label += mask
+            
         sitk.WriteImage(sitk.GetImageFromArray(combined_label), os.path.join(train_masks_path, i + ".png"))
     
     for i in os.listdir(os.path.join(ROOT, "datasets/raw", f"stage{stage}_test")):
@@ -150,17 +167,131 @@ def map_dataset(
         for j in sorted(glob.glob(os.path.join(ROOT, "datasets/raw", f"stage{stage}_test", i, "images", "*.png"))):
             shutil.copy(j, test_images_path)
     
-    return load_dataset(train_images_path, train_masks_path, test_images_path, test_masks_path)
+    return load_dataset(train_images_path, train_masks_path, test_images_path, test_masks_path, cfg)
 
+def convert_to_greyscale(image):
+    return image.convert('L')
 
 def load_dataset(
     train_images_path: str,
     train_masks_path: str,
     test_images_path: str,
-    test_masks_path: str
+    test_masks_path: str,
+    cfg: OmegaConf
 ) -> Tuple[DataLoader, DataLoader, DataLoader]:
-    # TODO: need to process and load dataset
-    tr_loader, val_loader, te_loader = None, None, None
+    # create a dic with directory of images and masks
+    train_files = [{"image": img, "label": mask} for img, mask in 
+                   zip(sorted(glob.glob(os.path.join(train_images_path, "*.png"))), 
+                       sorted(glob.glob(os.path.join(train_masks_path, "*.png"))))]
+    test_files = [{"image": img, "label": mask} for img, mask in 
+                  zip(sorted(glob.glob(os.path.join(test_images_path, "*.png"))), 
+                      sorted(glob.glob(os.path.join(test_masks_path, "*.png"))))]
+    # define transforms
+    train_transforms = Compose(
+        [
+        # Load image and label data
+        LoadImaged(keys=["image", "label"]), #pn files loaded as PIL image
+        # Ensure channel is the first dimension
+        EnsureChannelFirstd(keys=["image", "label"]),
+        # Convert images and masks to grey scale
+        Lambdad(keys=["image", "label"], func=convert_to_greyscale),
+        # resize images and masks with scaling
+        Resized(keys=["image", "label"], spatial_size=(256, 256), mode=("bilinear", "nearest")),
+        # Scale intensity values of the image within the specified range
+        ScaleIntensityRanged(
+            keys=["image"],
+            a_min=-57,
+            a_max=164,
+            b_min=0.0,
+            b_max=1.0,
+            clip=True,
+        ),
+        # Crop foreground from the image and label using the source image
+        # so that it will not learn the background
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        # Adjust the orientation of the image and label using RAS (Right, Anterior, Superior) axcodes
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        # Adjust the spacing of the image and label using specified pixel dimensions and interpolation modes
+        # this helps with data integration
+        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+        # Randomly crop regions with positive and negative labels to create training samples
+        RandCropByPosNegLabeld(
+            keys=["image", "label"],
+            label_key="label",
+            spatial_size=(96, 96, 96), # this will be the size of our cropped images to be inputted
+            pos=1,
+            neg=1,
+            num_samples=4,
+            image_key="image",
+            image_threshold=0,
+        ),
+        # random transforms
+        RandAffined(
+            keys=['image', 'label'],
+            mode=('bilinear', 'nearest'),
+            prob=1.0, spatial_size=(96, 96, 96),
+            rotate_range=(0, 0, np.pi/15),
+            scale_range=(0.1, 0.1, 0.1))
+        ]
+    )
+
+# Define validation and test transforms
+    val_transforms = Compose(
+        [
+            # Load image and label data
+            LoadImaged(keys=["image", "label"]),
+            # Ensure channel is the first dimension
+            EnsureChannelFirstd(keys=["image", "label"]),
+            # Convert images and masks to grey scale
+            Lambdad(keys=["image", "label"], func=convert_to_greyscale),
+            # resize images and masks with scaling
+            Resized(keys=["image", "label"], spatial_size=(256, 256), mode=("bilinear", "nearest")),
+            # Scale intensity values of the image within the specified range
+            ScaleIntensityRanged(
+                keys=["image"],
+                a_min=-57,
+                a_max=164,
+                b_min=0.0,
+                b_max=1.0,
+                clip=True,
+            ),
+            # Crop foreground from the image and label using the source image
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            # Adjust the orientation of the image and label using RAS (Right, Anterior, Superior) axcodes
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            # Adjust the spacing of the image and label using specified pixel dimensions and interpolation modes
+            Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+        ]
+    )
+
+    # load datasets
+    tran_size = int(0.8 * len(train_files))
+    val_size = len(train_files) - tran_size
+    train_data, test_data = torch.utils.data.random_split(train_files, [tran_size, val_size])
+    print(f"Training data size: {len(train_data)}")
+    print(f"Validation data size: {len(test_data)}")
+
+    tr_loader = DataLoader(
+        train_data,
+        batch_size=cfg.training.batch_size,
+        shuffle=True,
+        num_workers=cfg.training.num_workers,
+        transform=train_transforms
+    )
+    val_loader = DataLoader(
+        test_data,
+        batch_size=cfg.training.batch_size,
+        shuffle=False,
+        num_workers=cfg.training.num_workers,
+        transform=val_transforms
+    )
+    te_loader = DataLoader(
+        test_files,
+        batch_size=cfg.training.batch_size,
+        shuffle=False,
+        num_workers=cfg.training.num_workers,
+        transform=val_transforms
+    )
     
     return tr_loader, val_loader, te_loader
 
