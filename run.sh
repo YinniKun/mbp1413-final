f0f3541ad4156c1a6e7a3482e97f0a313a24767c
YinniKun <dongrichard123@gmail.com>
2024-02-23 13:10:52 -0500

    change running memory to enable faster queueing

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=180G
+#SBATCH --mem=100G
 #SBATCH --time=3-00:00:00
 #SBATCH --gres=gpu:v100:1
 #SBATCH --output=/project/6068045/zf2dong/mbp1413/mbp1413-final/outs/%x_%j.out
