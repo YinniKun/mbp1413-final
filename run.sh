cdd5bb7cc9b0425230fdab73b3fd9998c5888142
Richard D <94827377+YinniKun@users.noreply.github.com>
2024-03-02 13:14:44 -0500

    training for last batch of unetr only

--- run.sh
+++ run.sh
@@ -11,53 +11,53 @@
 
 source /project/6068045/zf2dong/mbp1413/env/pytorch/bin/activate
 
-echo "For UNet" 
-python -u main.py -c config.yaml -e 50 -l 0.001
-python -u main.py -c config.yaml -m "test" -e 50 -l 0.001
-python -u main.py -c config.yaml -e 100  -l 0.001
-python -u main.py -c config.yaml -m "test" -e 100  -l 0.001
-python -u main.py -c config.yaml -e 150 -l 0.001
-python -u main.py -c config.yaml -m "test" -e 150 -l 0.001
-python -u main.py -c config.yaml -e 200 -l 0.001
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.001
+# echo "For UNet" 
+# python -u main.py -c config.yaml -e 50 -l 0.001
+# python -u main.py -c config.yaml -m "test" -e 50 -l 0.001
+# python -u main.py -c config.yaml -e 100  -l 0.001
+# python -u main.py -c config.yaml -m "test" -e 100  -l 0.001
+# python -u main.py -c config.yaml -e 150 -l 0.001
+# python -u main.py -c config.yaml -m "test" -e 150 -l 0.001
+# python -u main.py -c config.yaml -e 200 -l 0.001
+# python -u main.py -c config.yaml -m "test" -e 200 -l 0.001
 
-python -u main.py -c config.yaml -e 50 -l 0.005
-python -u main.py -c config.yaml -m "test" -e 50 -l 0.005
-python -u main.py -c config.yaml -e 100  -l 0.005
-python -u main.py -c config.yaml -m "test" -e 100 -l 0.005
-python -u main.py -c config.yaml -e 150 -l 0.005
-python -u main.py -c config.yaml -m "test" -e 150 -l 0.005
-python -u main.py -c config.yaml -e 200 -l 0.005
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005
+# python -u main.py -c config.yaml -e 50 -l 0.005
+# python -u main.py -c config.yaml -m "test" -e 50 -l 0.005
+# python -u main.py -c config.yaml -e 100  -l 0.005
+# python -u main.py -c config.yaml -m "test" -e 100 -l 0.005
+# python -u main.py -c config.yaml -e 150 -l 0.005
+# python -u main.py -c config.yaml -m "test" -e 150 -l 0.005
+# python -u main.py -c config.yaml -e 200 -l 0.005
+# python -u main.py -c config.yaml -m "test" -e 200 -l 0.005
 
-python -u main.py -c config.yaml -e 50 -l 0.01
-python -u main.py -c config.yaml -m "test" -e 50 -l 0.01
-python -u main.py -c config.yaml -e 100  -l 0.01
-python -u main.py -c config.yaml -m "test" -e 100 -l 0.01
-python -u main.py -c config.yaml -e 150 -l 0.01
-python -u main.py -c config.yaml -m "test" -e 150 -l 0.01
-python -u main.py -c config.yaml -e 200 -l 0.01
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.01
+# python -u main.py -c config.yaml -e 50 -l 0.01
+# python -u main.py -c config.yaml -m "test" -e 50 -l 0.01
+# python -u main.py -c config.yaml -e 100  -l 0.01
+# python -u main.py -c config.yaml -m "test" -e 100 -l 0.01
+# python -u main.py -c config.yaml -e 150 -l 0.01
+# python -u main.py -c config.yaml -m "test" -e 150 -l 0.01
+# python -u main.py -c config.yaml -e 200 -l 0.01
+# python -u main.py -c config.yaml -m "test" -e 200 -l 0.01
 
-echo "Done for UNet"
-echo "For UNetR"
-python -u main.py -c config.yaml -e 50 -l 0.001 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 50 -l 0.001 -mo "unetr"
-python -u main.py -c config.yaml -e 100  -l 0.001 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 100  -l 0.001 -mo "unetr"
-python -u main.py -c config.yaml -e 150 -l 0.001 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 150 -l 0.001 -mo "unetr"
-python -u main.py -c config.yaml -e 200 -l 0.001 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.001 -mo "unetr"
+# echo "Done for UNet"
+# echo "For UNetR"
+# python -u main.py -c config.yaml -e 50 -l 0.001 -mo "unetr"
+# python -u main.py -c config.yaml -m "test" -e 50 -l 0.001 -mo "unetr"
+# python -u main.py -c config.yaml -e 100  -l 0.001 -mo "unetr"
+# python -u main.py -c config.yaml -m "test" -e 100  -l 0.001 -mo "unetr"
+# python -u main.py -c config.yaml -e 150 -l 0.001 -mo "unetr"
+# python -u main.py -c config.yaml -m "test" -e 150 -l 0.001 -mo "unetr"
+# python -u main.py -c config.yaml -e 200 -l 0.001 -mo "unetr"
+# python -u main.py -c config.yaml -m "test" -e 200 -l 0.001 -mo "unetr"
 
-python -u main.py -c config.yaml -e 50 -l 0.005 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 50 -l 0.005 -mo "unetr"
-python -u main.py -c config.yaml -e 100  -l 0.005 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 100 -l 0.005 -mo "unetr"
-python -u main.py -c config.yaml -e 150 -l 0.005 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 150 -l 0.005 -mo "unetr"
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr"
+# python -u main.py -c config.yaml -e 50 -l 0.005 -mo "unetr"
+# python -u main.py -c config.yaml -m "test" -e 50 -l 0.005 -mo "unetr"
+# python -u main.py -c config.yaml -e 100  -l 0.005 -mo "unetr"
+# python -u main.py -c config.yaml -m "test" -e 100 -l 0.005 -mo "unetr"
+# python -u main.py -c config.yaml -e 150 -l 0.005 -mo "unetr"
+# python -u main.py -c config.yaml -m "test" -e 150 -l 0.005 -mo "unetr"
+# python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr"
+# python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr"
 
 python -u main.py -c config.yaml -e 50 -l 0.01 -mo "unetr"
 python -u main.py -c config.yaml -m "test" -e 50 -l 0.01 -mo "unetr"
