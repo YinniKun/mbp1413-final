975ad65c4547e5c593826fd4c93d801d233369de
YinniKun <dongrichard123@gmail.com>
2024-03-14 20:04:09 -0400

    update training script

--- run.sh
+++ run.sh
@@ -1,73 +1,76 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=run-model
+#SBATCH --job-name=train-model-all
 #SBATCH --account=def-gregorys
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=80G
-#SBATCH --time=20:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --gres=gpu:v100:1
 #SBATCH --output=/project/6068045/zf2dong/mbp1413/mbp1413-final/outs/%x_%j.out
 
 source /project/6068045/zf2dong/mbp1413/env/pytorch/bin/activate
 
-# echo "For UNet" 
-# python -u main.py -c config.yaml -e 50 -l 0.001
-# python -u main.py -c config.yaml -m "test" -e 50 -l 0.001
-# python -u main.py -c config.yaml -e 100  -l 0.001
-# python -u main.py -c config.yaml -m "test" -e 100  -l 0.001
-# python -u main.py -c config.yaml -e 150 -l 0.001
-# python -u main.py -c config.yaml -m "test" -e 150 -l 0.001
-# python -u main.py -c config.yaml -e 200 -l 0.001
-# python -u main.py -c config.yaml -m "test" -e 200 -l 0.001
-
-# python -u main.py -c config.yaml -e 50 -l 0.005
-# python -u main.py -c config.yaml -m "test" -e 50 -l 0.005
-# python -u main.py -c config.yaml -e 100  -l 0.005
-# python -u main.py -c config.yaml -m "test" -e 100 -l 0.005
-# python -u main.py -c config.yaml -e 150 -l 0.005
-# python -u main.py -c config.yaml -m "test" -e 150 -l 0.005
-# python -u main.py -c config.yaml -e 200 -l 0.005
-# python -u main.py -c config.yaml -m "test" -e 200 -l 0.005
-
-# python -u main.py -c config.yaml -e 50 -l 0.01
-# python -u main.py -c config.yaml -m "test" -e 50 -l 0.01
-# python -u main.py -c config.yaml -e 100  -l 0.01
-# python -u main.py -c config.yaml -m "test" -e 100 -l 0.01
-# python -u main.py -c config.yaml -e 150 -l 0.01
-# python -u main.py -c config.yaml -m "test" -e 150 -l 0.01
-# python -u main.py -c config.yaml -e 200 -l 0.01
-# python -u main.py -c config.yaml -m "test" -e 200 -l 0.01
-
-# echo "Done for UNet"
-# echo "For UNetR"
-# python -u main.py -c config.yaml -e 50 -l 0.001 -mo "unetr"
-# python -u main.py -c config.yaml -m "test" -e 50 -l 0.001 -mo "unetr"
-# python -u main.py -c config.yaml -e 100  -l 0.001 -mo "unetr"
-# python -u main.py -c config.yaml -m "test" -e 100  -l 0.001 -mo "unetr"
-# python -u main.py -c config.yaml -e 150 -l 0.001 -mo "unetr"
-# python -u main.py -c config.yaml -m "test" -e 150 -l 0.001 -mo "unetr"
-# python -u main.py -c config.yaml -e 200 -l 0.001 -mo "unetr"
-# python -u main.py -c config.yaml -m "test" -e 200 -l 0.001 -mo "unetr"
-
-# python -u main.py -c config.yaml -e 50 -l 0.005 -mo "unetr"
-# python -u main.py -c config.yaml -m "test" -e 50 -l 0.005 -mo "unetr"
-# python -u main.py -c config.yaml -e 100  -l 0.005 -mo "unetr"
-# python -u main.py -c config.yaml -m "test" -e 100 -l 0.005 -mo "unetr"
-# python -u main.py -c config.yaml -e 150 -l 0.005 -mo "unetr"
-# python -u main.py -c config.yaml -m "test" -e 150 -l 0.005 -mo "unetr"
-# python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr"
-# python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr"
-
 echo "Exp 2"
-python -u main.py -c config.yaml -e 200 -l 0.001 -mo "unetr" -opt "Adam" -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.001 -mo "unetr" -opt "Adam" -sch
+echo "Training and testing UNet"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "Adam" -sch
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "Adam" -sch
+echo "Training and testing UNeTr"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "Adam" -sch
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -sch
 
-python -u main.py -c config.yaml -m "test" -e 50 -l 0.01 -mo "unetr"
-python -u main.py -c config.yaml -e 100  -l 0.01 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 100 -l 0.01 -mo "unetr"
-python -u main.py -c config.yaml -e 150 -l 0.01 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 150 -l 0.01 -mo "unetr"
-python -u main.py -c config.yaml -e 200 -l 0.01 -mo "unetr"
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.01 -mo "unetr"
-echo "Done for UNetR"
+echo "Exp 3"
+echo "Training and testing UNet"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "Adam" -no
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "Adam" -no
+echo "Training and testing UNeTr"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no
+
+echo "Exp 4"
+echo "Training and testing UNet"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "Adam" -no -sch
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "Adam" -no -sch
+echo "Training and testing UNeTr"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no -sch
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no -sch
+
+echo "Exp 5"
+echo "Training and testing UNet"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "SGD" 
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "SGD"
+echo "Training and testing UNeTr"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "SGD" 
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam"
+
+echo "Exp 6"
+echo "Training and testing UNet"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "SGD" -sch
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "SGD" -sch
+echo "Training and testing UNeTr"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "SGD" -sch
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -sch
+
+echo "Exp 7"
+echo "Training and testing UNet"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "SGD" -no
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "SGD" -no
+echo "Training and testing UNeTr"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "SGD" -no
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no
+
+echo "Exp 8"
+echo "Training and testing UNet"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "SGD" -no -sch
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "SGD" -no -sch
+echo "Training and testing UNeTr"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "SGD" -no -sch
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no -sch
+
+echo "Exp 1"
+echo "Training and testing UNet"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "Adam"
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "Adam"
+echo "Training and testing UNeTr"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "Adam"
+python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam"
