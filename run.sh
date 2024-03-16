362f294238242a178098be865437e2181a407a8c
Chris Xiao <yl.xiao@mail.utoronto.ca>
2024-03-15 22:01:53 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,112 +14,112 @@ source /project/6068045/zf2dong/mbp1413/env/pytorch/bin/activate
 echo "Exp 2"
 echo "Training and testing UNet"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "Adam" -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "Adam" -sch
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unet -opt Adam -sch
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unet -opt Adam -sch
 end=`date +%s`
 runtime2_1=$((end-start))
 echo "Training and testing UNeTr"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "Adam" -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -sch
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unetr -opt Adam -sch
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unetr -opt Adam -sch
 end=`date +%s`
 runtime2_2=$((end-start))
 
 echo "Exp 3"
 echo "Training and testing UNet"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "Adam" -no
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "Adam" -no
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unet -opt Adam -no
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unet -opt Adam -no
 end=`date +%s`
 runtime3_1=$((end-start))
 echo "Training and testing UNeTr"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unetr -opt Adam -no
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unetr -opt Adam -no
 end=`date +%s`
 runtime3_2=$((end-start))
 
 echo "Exp 4"
 echo "Training and testing UNet"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "Adam" -no -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "Adam" -no -sch
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unet -opt Adam -no -sch
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unet -opt Adam -no -sch
 end=`date +%s`
 runtime4_1=$((end-start))
 echo "Training and testing UNeTr"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam" -no -sch
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unetr -opt Adam -no -sch
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unetr -opt Adam -no -sch
 end=`date +%s`
 runtime4_2=$((end-start))
 
 echo "Exp 5"
 echo "Training and testing UNet"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "SGD" 
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "SGD"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unet -opt SGD 
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unet -opt SGD
 end=`date +%s`
 runtime5_1=$((end-start))
 echo "Training and testing UNeTr"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "SGD" 
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "SGD"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unetr -opt SGD 
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unetr -opt SGD
 end=`date +%s`
 runtime5_2=$((end-start))
 
 echo "Exp 6"
 echo "Training and testing UNet"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "SGD" -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "SGD" -sch
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unet -opt SGD -sch
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unet -opt SGD -sch
 end=`date +%s`
 runtime6_1=$((end-start))
 echo "Training and testing UNeTr"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "SGD" -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "SGD" -sch
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unetr -opt SGD -sch
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unetr -opt SGD -sch
 end=`date +%s`
 runtime6_2=$((end-start))
 
 echo "Exp 7"
 echo "Training and testing UNet"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "SGD" -no
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "SGD" -no
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unet -opt SGD -no
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unet -opt SGD -no
 end=`date +%s`
 runtime7_1=$((end-start))
 echo "Training and testing UNeTr"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "SGD" -no
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "SGD" -no
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unetr -opt SGD -no
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unetr -opt SGD -no
 end=`date +%s`
 runtime7_2=$((end-start))
 
 echo "Exp 8"
 echo "Training and testing UNet"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "SGD" -no -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "SGD" -no -sch
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unet -opt SGD -no -sch
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unet -opt SGD -no -sch
 end=`date +%s`
 runtime8_1=$((end-start))
 echo "Training and testing UNeTr"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "SGD" -no -sch
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "SGD" -no -sch
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unetr -opt SGD -no -sch
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unetr -opt SGD -no -sch
 end=`date +%s`
 runtime8_2=$((end-start))
 
 echo "Exp 1"
 echo "Training and testing UNet"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unet" -opt "Adam"
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unet" -opt "Adam"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unet -opt Adam
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unet -opt Adam
 end=`date +%s`
 runtime1_1=$((end-start))
 echo "Training and testing UNeTr"
 start=`date +%s`
-python -u main.py -c config.yaml -e 200 -l 0.005 -mo "unetr" -opt "Adam"
-python -u main.py -c config.yaml -m "test" -e 200 -l 0.005 -mo "unetr" -opt "Adam"
+python -u main.py -c config.yaml -e 200 -l 0.005 -mo unetr -opt Adam
+python -u main.py -c config.yaml -m test -e 200 -l 0.005 -mo unetr -opt Adam
 end=`date +%s`
 runtime1_2=$((end-start))
 
